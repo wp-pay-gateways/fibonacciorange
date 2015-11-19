dabc2e77b150a9f88bbcd3f8eda0497e4ae49f13
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 11:51:10 +0100

    Extend from ICEPAY library.

--- src/Integration.php
+++ src/Integration.php
@@ -1,21 +1,9 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_FibonacciOrange_Integration {
+class Pronamic_WP_Pay_Gateways_FibonacciOrange_Integration extends Pronamic_WP_Pay_Gateways_Icepay_Integration {
 	public function __construct() {
 		$this->id       = 'fibonacciorange';
 		$this->name     = 'Fibonacci ORANGE';
 		$this->provider = 'fibonacciorange';
-	}
-
-	public function get_config_factory_class() {
-		return 'Pronamic_WP_Pay_Gateways_Icepay_ConfigFactory';
-	}
-
-	public function get_config_class() {
-		return 'Pronamic_WP_Pay_Gateways_Icepay_Config';
-	}
-
-	public function get_gateway_class() {
-		return 'Pronamic_WP_Pay_Gateways_Icepay_Gateway';
 	}
 }
