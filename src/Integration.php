dcb568e22b723e15123d3348544062b8b0f94dcf
Remco Tolsma <info@remcotolsma.nl>
2016-03-22 14:09:28 +0100

    Getting ready for version 1.0.1.

--- src/Integration.php
+++ src/Integration.php
@@ -7,7 +7,8 @@
  * Company: Pronamic
  *
  * @author Remco Tolsma
- * @version 1.0.0
+ * @version 1.0.1
+ * @since 1.0.0
  */
 class Pronamic_WP_Pay_Gateways_FibonacciOrange_Integration extends Pronamic_WP_Pay_Gateways_Icepay_Integration {
 	public function __construct() {
